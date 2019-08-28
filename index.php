1b3ec44daf0ed06fafb3257f2ce588a9a3361aca
JonayMedina7 <jin.rock.and1@gmail.com>
2019-08-28 14:37:35 -0430

    finish index and services

---
+++ index.php
@@ -0,0 +1,299 @@
+<?php include './incl/inth.php'; ?>
+    <!-- END nav -->
+    <section class="home-slider owl-carousel">
+        <div class="slider-item" style="background-image:url(images/bn_1.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+                    <div class="col-md-8 text-center ftco-animate">
+                        <h1 class="mb-4">We Offer <span>containers for Industrial and Domestic use, New and Recycled.</span></h1>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slider-item" style="background-image:url(images/bn_2.jpg);">
+            <div class="overlay"></div>
+                <div class="container">
+                  <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+                      <div class="col-md-8 text-center ftco-animate">
+                        <h1 class="mb-4">We are a company in Pro-environment <span> with ecological recycling systems.</span></h1>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="ftco-section ftco-no-pt ftc-no-pb">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 wrap-about py-5 wrap-about bg-light">
+                    <div class="text px-4 ftco-animate">
+                        <h2 class="mb-4 title-lgreen">Welcome to Recycling World</h2>
+                        <p>Recycling USA Inc. Brings unparalleled packaging performance and innovation to redefine sustainability for customers. From new packaging made from recycled content, to reconditioning, reuse, recycling and professional disposal, we provide customized solutions that have a positive impact on businesses and the planet, bringing true sustainability at scale to companies all over the world.</p>
+                        <img class="img img-fluid pb-3" src="images/superpail.jpg">
+                        <!-- <a href="#" class="btn btn-secondary px-4 py-3">Read More</a> -->
+                    </div>
+                </div>
+                <div class="col-md-7 order-md-last wrap-about py-5 pr-md-4 ftco-animate">
+                    <h2 class="mb-4 title-lblue">What We Offer</h2>
+                    <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
+                    <div class="row mt-5">
+
+                        <div class="col-lg-6">
+                            <div class="services-2 d-flex">
+                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center">
+                                    <span class="flaticon-reading"></span>
+                                </div>
+                                <div class="text">
+                                    <h3>Eco-Friendly</h3>
+                                    <p>Applying low impact reconditioning systems to the environment.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="services-2 d-flex">
+                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center">
+                                    <span class="flaticon-diploma"></span>
+                                </div>
+                                <div class="text">
+                                    <h3>Quality</h3>
+                                    <p>Our products are manufactured and reconditioned under the best manufacturing and cleaning standards.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section class="ftco-section">
+        <div class="container-wrap">
+            <div class="row no-gutters">
+                <div class="container">
+                    <div class="row">
+
+
+                    <div class="col-md-12 text-center heading-section ftco-animate">
+                        <h2 class="mb-4 "><span>Our Services</span></h2>
+                    </div>
+
+                <div class="col-md-6 d-flex services align-self-stretch pb-4 px-4 ftco-animate">
+                    <div class="media block-6 d-flex text-center">
+                        <div class=" p-2 d-flex mt-3 justify-content-center align-items-center">
+                            <!-- <span class="flaticon-teacher"></span> -->
+                            <img class="img img-fluid pb-3" src="images/RECI4.jpg">
+                        </div>
+                        <div class="text p-2 mt-3">
+                            <h3 class="heading">Cleaning and Renovate</h3>
+                            <p>Reconditioning and return programs are available through Recycling USA Inc.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex services align-self-stretch pb-4 px-4 ftco-animate ">
+                    <div class="media block-6 d-flex text-center">
+                        <div class="text p-2 mt-3 align-items-stretch">
+                            <h3 class="heading">Collection of Empty Packaging</h3>
+                            <p>Collection of used empty packaging at production plants and customer sites.</p>
+                        </div>
+                        <div class="icon p-2 d-flex mt-3 justify-content-center align-items-stretch">
+                            <!-- <span class="flaticon-reading"></span> -->
+                            <img class="img img-fluid pb-3" src="images/RECI3.jpg">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex services align-self-stretch pb-4 px-4 ftco-animate">
+                    <div class="media block-6 d-flex text-center">
+                        <div class="icon p-2 d-flex mt-3 justify-content-center align-items-stretch">
+                            <!-- <span class="flaticon-books"></span> -->
+                            <img class="img img-fluid pb-3" src="images/RECI2.jpg">
+                        </div>
+                        <div class="text p-2 mt-3 align-items-stretch">
+                            <h3 class="heading">PickUp and Disposal Service</h3>
+                            <p>For pick up of reusable bung type plastic or steel drums, Ibc, Pails and Tight-Head Containers & Jerrycans.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex services align-self-stretch pb-4 px-4 ftco-animate">
+                    <div class="media block-6 d-flex text-center">
+                        <div class=" text p-2 mt-3 align-items-stretch">
+                            <h3 class="heading">Delivery Services</h3>
+                            <p>We can provide the logistical systems and expertise to help you manage your container fleet.</p>
+                        </div>
+                        <div class="icon p-2 mt-3 d-flex justify-content-center align-items-stretch">
+                            <!-- <span class="flaticon-diploma"></span> -->
+                            <img class="img img-fluid pb-3" src="images/RECI1.jpg">
+                        </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/bg_4.jpg);" data-stellar-background-ratio="0.5">
+       <div class="container">
+            <div class="row justify-content-center mb-5 pb-2">
+                <div class="col-md-8 text-center heading-section heading-section-black ftco-animate">
+                    <h2 class="mb-4"><span>20 Years of</span> Experience</h2>
+                    <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+                </div>
+            </div>
+            <div class="row d-md-flex align-items-center justify-content-center">
+                <div class="col-lg-10">
+                    <div class="row d-md-flex align-items-center">
+                        <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18">
+                                <div class="icon"><span class="flaticon-doctor"></span></div>
+                                 <div class="text">
+                                    <strong class="number" data-number="18">0</strong>
+                                    <span>Certified Teachers</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18">
+                                <div class="icon"><span class="flaticon-doctor"></span></div>
+                                <div class="text">
+                                    <strong class="number" data-number="351">0</strong>
+                                    <span>Successful Kids</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18">
+                                <div class="icon"><span class="flaticon-doctor"></span></div>
+                                <div class="text">
+                                    <strong class="number" data-number="564">0</strong>
+                                    <span>Happy Parents</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18">
+                                <div class="icon"><span class="flaticon-doctor"></span></div>
+                                <div class="text">
+                                    <strong class="number" data-number="300">0</strong>
+                                    <span>Awards Won</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<section class="ftco-section ftco-consult ftco-no-pt ftco-no-pb" >
+   <div class="container">
+      <div class="row justify-content-end">
+        <div class="col-md-6 py-5 px-md-5" style="background-repeat: no-repeat;background-size: cover; background-image: url(images/contact3f.jpg);"></div>
+         <div class="col-md-6 py-5 px-md-5 bg-primary">
+             <div class="heading-section heading-section-white ftco-animate mb-5">
+               <h2 class="mb-4">Request A Quote</h2>
+               <p>Write us to assist you in any concern, information that do you need.</p>
+           </div>
+           <form action="#" class="appointment-form ftco-animate">
+                   <div class="d-md-flex">
+                      <div class="form-group">
+                         <input type="text" class="form-control" placeholder="First Name">
+                     </div>
+                     <div class="form-group ml-md-4">
+                         <input type="text" class="form-control" placeholder="Last Name">
+                     </div>
+                 </div>
+                 <div class="d-md-flex">
+                        <div class="form-group">
+                            <div class="form-field">
+                                <div class="select-wrap">
+                                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                    <select name="" id="" class="form-control">
+                                       <option value="">Select Your Course</option>
+                                       <option value="products">Products</option>
+                                       <option value="service">Services</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group ml-md-4">
+                            <input type="text" class="form-control" placeholder="Phone">
+                        </div>
+                    </div>
+                        <div class="d-md-flex">
+                            <div class="form-group">
+                                <textarea name="" id="" cols="30" rows="2" class="form-control" placeholder="Message"></textarea>
+                            </div>
+                            <div class="form-group ml-md-4">
+                                <input type="submit" value="Request A Quote" class="btn btn-secondary py-3 px-4">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md-6 col-lg-3">
+                    <div class="ftco-footer-widget mb-5">
+                        <h2 class="ftco-heading-2">Have a Questions?</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@recyclingdrums.com</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="ftco-footer-widget mb-5 ml-md-4">
+                        <h2 class="ftco-heading-2">Links</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                            <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
+                            <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
+                            <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Deparments</a></li>
+                            <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+
+                    <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="icon-heart" aria-hidden="true"></i> by <a href="dilia.com.ve" target="_blank">Dilia Soluciones</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+<!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+
+    <script src="js/main.js"></script>
+
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,93 @@
+<?php
+ include './incl/inth.php';
+?>
+
+
+    <!-- END nav -->
+
+    <section class="hero-wrap hero-wrap-2" style="background-image:url(images/horizont.jpg)">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Services</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Services <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+		<section class="ftco-section">
+
+    	<div class="container">
+    		<div class="row">
+          <!-- service cleaning -->
+          <div class="col-md-12  d-flex services align-self-stretch pb-4 px-4 ftco-animate">
+            <div class="media bg-light block-6 d-flex text-center">
+              <div class="  col-md-6  mt-3 justify-content-center align-items-center">
+                <img class="pt-2 mt-sm-2" src="images/RECI4.jpg">
+              </div>
+              <div class="text bg-medium col-md-6 p-2 mt-3">
+                <h3 class="heading">Cleaning and Renovate</h3>
+                <p>First the drums are unloaded, opened and inspected. The drums are then loaded on a progressive washer that takes them through stages of washing, rinsing and drying. The drums are then inspected. If the drums pass inspection they are capped and their labels are removed. The drums are then blasted to remove all exterior coatings and identifying marks. The drums are then inspected for any exterior imperfections.
+                If any drums do not pass any of our three inspections or is unable to meet the current UN regulations they are washed, crushed, and recycled into a raw steel resource.</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- service colection -->
+          <div class="col-md-12 d-flex services align-self-stretch pb-4 px-4 ftco-animate ">
+            <div class="media bg-light block-6 d-flex text-center">
+              <div class="text bg-medium col-md-6 p-2 mt-3">
+                <h3 class="heading">Collection of Empty Packaging</h3>
+                <p>The more uses you can get out of your drums and IBCs, the lighter the impact on your operations&mdash;and the environment.<br/>To help you get the most out of your packaging, Mauser Packaging Solutions offers reconditioning services to customer-owned IBCs and drums. <br/>This full-service option includes:</p>
+                <ul>
+                  <li>Collection of empty packaging at customer sites or final customer locations.</li>
+                  <li>Laundry service of packaging, including rebottling, washing, and furnace oxidation.</li>
+                  <li>Drying and testing of packaging.</li>
+                  <li>Minor IBC and drum repair.</li>
+                  <li>Return of packaging to customer filling operations centers.</li>
+                </ul>
+              </div>
+              <div class=" col-md-6  mt-3 justify-content-center align-items-center">
+                <img class="pt-5 mt-sm-5" src="images/RECI3.jpg">
+              </div>
+            </div>
+          </div>
+
+        	<div class="col-md-12 d-flex services align-self-stretch pb-4 px-4 ftco-animate ">
+            <div class="media bg-light block-6 d-flex text-center">
+              <div class=" col-md-6  mt-3 justify-content-center align-items-center">
+                <img class="pt-2 mt-sm-2" src="images/RECI2.jpg">
+              </div>
+              <div class="pt-5 mt-sm-5 text bg-medium col-md-6 p-2 mt-3">
+                <h3 class="heading">PickUp and Disposal Service</h3>
+                <p>Service dedicated to all drums and containers that need to be picked up, washed, de-identified and scrapped. Caution: All drums and containers will be refused if they do not have closures in place and are not empty according.
+              </div>
+            </div>
+          </div>
+        	<div class="col-md-12 d-flex services align-self-stretch pb-4 px-4 ftco-animate ">
+            <div class="media bg-light block-6 d-flex text-center">
+              <div class="text bg-medium col-md-6 p-2 mt-3">
+                <h3 class="heading">Delivery Services</h3>
+                <p>We can provide the logistical systems and expertise to help you manage your container fleet.</p>
+                <p>Services include:</p>
+                <ul>
+                    <li>Fleet tracking/supply chain management.</li>
+                    <li>Interior and exterior vehicle cleaning.</li>
+                    <li>UN/DOT retesting and certification.</li>
+                    <li>Transportation.</li>
+                    <li>Storage.</li>
+                </ul>
+              </div>
+              <div class=" col-md-6  mt-3 justify-content-center align-items-center">
+                <img class="p-1 m-1" src="images/RECI1.jpg">
+              </div>
+            </div>
+          </div>
+
+        </div>
+    	</div>
+    </section>
+
+
+<?php include './incl/intf.php'; ?>
